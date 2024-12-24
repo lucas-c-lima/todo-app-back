005a7724127a18733300b8c8e567c78e4c94e153
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2024-12-24 10:18:25 -0300

    Post Method Created

---
+++ Models/ToDoRequest.cs
@@ -0,0 +1,1 @@
+public record ToDoRequest(string name);
--- Routes/ToDoRoute.cs
+++ Routes/ToDoRoute.cs
@@ -3,6 +3,14 @@ public static class ToDoRoute
 {
     public static void ToDoRoutes(this WebApplication app)
     {
-        app.MapGet("person", () => new ToDoModel("Nome Person"));
+        var route = app.MapGroup("person");
+
+        route.MapPost("",
+            async (ToDoRequest req, ToDoContext context) =>
+        {
+            var task = new ToDoModel(req.name);
+            await context.AddAsync(task);
+            await context.SaveChangesAsync();
+        });
     }
 }
